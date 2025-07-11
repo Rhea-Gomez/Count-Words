a380fe1d525d3bf54e8538fb19c96f0a285d9ea2
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 16:23:36 +0530

    Assignment on Counting words

---
+++ CountWords/CountWords/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CountWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWords;
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            var words = sentence.Split(' ');
+
+            numberOfWords = words.Length;
+
+            Console.WriteLine("Total words: " + numberOfWords);
+            
+        }
+    }
+}
